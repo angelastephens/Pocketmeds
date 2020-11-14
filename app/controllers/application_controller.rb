96df54a2527b1bf9a712607108a649830a7944be
angela stephens <angelastephens317@gmail.com>
2020-11-14 10:46:22 -0500

    add logged in and auth check methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
     def current_user
         User.find_by_id(session[:id])  
     end 
+
+    def logged_in
+     !!current_user 
+    end 
+
+    def auth_check
+        if !logged_in
+            flash[:error] = "You must be logged in to view this page"
+        redirect_to root_path   
+        end 
+    end 
 end
