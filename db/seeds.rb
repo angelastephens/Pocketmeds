7993e884cabd12bd21676a8ff2c36feb78bb2e7b
angela stephens <angelastephens317@gmail.com>
2020-11-15 22:56:05 -0500

    add nokogiri to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+
+def medication_index
+    doc = Nokogiri::HTML(open("https://www.iodine.com/drug"))
+    doc.css('section ul li.grow-none').map {|med| med.text }
+    
+  
+end
+
+medication_index.each do |medication|
+    Medication.find_or_create_by(name: medication)
+end
