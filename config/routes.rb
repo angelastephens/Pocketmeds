542d13f86948be6902a67bf7d2653bccc2fc6dd4
angela stephens <angelastephens317@gmail.com>
2020-11-15 22:54:14 -0500

    update routes and nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do 
+  get 'medications/index'
+  post 'sessions/create'
+  delete 'sessions/destroy'
+  get 'auth/:provider/callback', to: 'omniauth#create'
+  root 'static#new'
+
+  resources :users, only:[:create, :show, :edit, :update, :destroy]
+  
+  resources :refills, only:[:create, :index] 
+
+
+  resources  :prescriptions do
+    resources :refills
+  
+  end
+  
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
