9b7ab4e6e9ea0bcd43a8a562f398ca5c1c6a0d79
angela stephens <angelastephens317@gmail.com>
2020-11-15 22:50:21 -0500

    add validates

--- app/models/refill.rb
+++ app/models/refill.rb
@@ -1,6 +1,9 @@
 class Refill < ApplicationRecord
     belongs_to :prescription, counter_cache: :refill_count
     validate :refill_counter, :on => :create 
+    validates :dispensed_date, presence: true
+    validates :expiration_date,  presence: true
+    
 
     def refill_counter
         if self.prescription.refills.count >= self.prescription.refill_amount
