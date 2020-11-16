f2ce020dd081d686ab8c8357204796daccc7d775
angela stephens <angelastephens317@gmail.com>
2020-11-15 22:49:40 -0500

    add validations

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -2,4 +2,7 @@ class Prescription < ApplicationRecord
     belongs_to :user 
     belongs_to :medication
     has_many :refills
+    validates :refill_amount, presence: true
+    validates :dosage, presence: true
+    validates :quantity, presence: true 
 end
