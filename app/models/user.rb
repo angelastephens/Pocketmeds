0d82ac7540d8a6fe2a7cc12add6c7a3ee9c98399
angela stephens <angelastephens317@gmail.com>
2020-11-19 21:19:49 -0500

    add note

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_many :prescriptions
     has_many :medications, -> {distinct}, through: :prescriptions
     has_many :refills, through: :prescriptions
-    has_secure_password 
+    has_secure_password   #macros gives authentica method, password equals and validates presences of password
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates :phone_number, presence: true
