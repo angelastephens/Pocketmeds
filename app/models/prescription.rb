b4bcda51e6f86439f3633e028da5b0289ec4790f
angela stephens <angelastephens317@gmail.com>
2020-11-19 21:19:40 -0500

    add scope method

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -6,5 +6,7 @@ class Prescription < ApplicationRecord
     validates :dosage, presence: true
     validates :quantity, presence: true 
 
-   
+    def self.alphabetical # scope to return medcations in alphabetical order
+        Prescription.includes(:medication).order("medications.name ASC") #includes sql query that pulls prescriptions AND their names instead of two querys)
+    end
 end
